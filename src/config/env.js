1865e355f7b34375c23061464196731cf70d9869
ElemenX-D <elemenx@iot-dashboard-be-admin.dd>
2020-08-03 18:16:30 +0800

    update

--- src/config/env.js
+++ src/config/env.js
@@ -11,7 +11,7 @@ let routerMode = 'hash';
 let baseImgPath;
 
 if (process.env.NODE_ENV == 'development') {
-    baseUrl = '';
+    baseUrl = 'http://onedrive-be.dd/api';
     baseImgPath = '/img/';
 } else {
     baseUrl = '//elm.cangdu.org';
