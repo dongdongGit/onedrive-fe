39cc2c5261ed374456e43b59f3040c40e9ef2ba2
ElemenX-D <elemenx@iot-dashboard-be-admin.dd>
2020-08-03 15:10:47 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -30,3 +30,9 @@
   }
 }
 </style>
+
+<script>
+    export default {
+    	
+    }
+</script>
